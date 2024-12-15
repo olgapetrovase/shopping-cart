809a50fb913c44c0175f0d47b6918202edc2705b
Olha Petrova <olga.petrova.se@gmail.com>
2024-12-15 13:42:04 -0800

    Consumer approach

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 
 import CartModal from './CartModal.jsx';
+import { CartContext } from '../store/shopping-cart-context.jsx';
 
-export default function Header({ cart, onUpdateCartItemQuantity }) {
+export default function Header({ onUpdateCartItemQuantity }) {
   const modal = useRef();
 
-  const cartQuantity = cart.items.length;
+  const { items } = useContext(CartContext);
+
+  const cartQuantity = items.length;
 
   function handleOpenCartClick() {
     modal.current.open();
@@ -26,7 +29,7 @@ export default function Header({ cart, onUpdateCartItemQuantity }) {
     <>
       <CartModal
         ref={modal}
-        cartItems={cart.items}
+        cartItems={items}
         onUpdateCartItemQuantity={onUpdateCartItemQuantity}
         title="Your Cart"
         actions={modalActions}
